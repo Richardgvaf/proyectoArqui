347df9ff2a61b68692ac5cdff196cf0390d09ac0
Richardgvaf <noticiasnuevayo@gmail.com>
2020-05-28 14:45:37 -0600

    update with macros save

--- saveData.asm
+++ saveData.asm
@@ -1,16 +1,8 @@
 %macro escribe 2
-mov eax, 4
-mov ebx, 1
-mov ecx, %1
-mov edx, %2
-int 0x80
-%endmacro
-
-%macro leeTxt 3
-	mov eax, 3
-	mov ebx, %1
-	mov ecx, %2
-	mov edx, %3
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, %1
+	mov edx, %2
 	int 0x80
 %endmacro
 %macro escribeTxt 3
@@ -20,6 +12,12 @@ int 0x80
 	mov edx, %3
 	int 0x80
 %endmacro
+%macro abrirArchivo 2
+	mov eax, %1
+	mov ebx, %2
+	mov edx,7777h
+	int 0x80
+%endmacro
 %macro cerrarArchivo 1
 	mov eax, 6
 	mov ebx, %1
@@ -27,90 +25,46 @@ int 0x80
 	mov edx, 0
 	int 0x80
 %endmacro
-%macro cargar 2
-	mov eax, %1
-	mov ebx, %2
-	mov ecx, 2
-	mov edx, 7777h
-	int 0x80
-
-%endmacro
 
 segment .data
+msg2 db "leyendo desde un archivo :",0xA
+len2 equ $-msg2
 
-aviso db "Escribe en archivo :",0xA
-lenaviso equ $-aviso
 archivo db "/home/richard/ensamblador/archivo.txt",0
 
 
-segment .bss
-texto resb 3
-idarchivo resd 1
-txtprint  resd 10
 
+segment .bss
+texto resb 4
+idarchivo resd 1
 
 segment .text
-
-
-
 
 global _start
 
 _start:
 
-
-;mov eax, 8
-;mov ebx, archivo
-;mov ecx, 2
-;mov edx, 7777h
-;int 0x80
-cargar 8,archivo
-test eax, eax
-jz salir
-mov dword[idarchivo], eax			;accedemos al archivo
-
-escribe aviso, lenaviso
+abrirArchivo 8,archivo    ;ponemos modo edicion y archivo destino; carga el archivo o lo crea
 
 
-mov eax,'012'
-mov [texto],eax
-escribeTxt [idarchivo],texto,3
-
-
-mov eax,'abc'
-mov [texto],eax
-escribeTxt [idarchivo],texto,3
-
-
-;escribe idarchivo,1
-;cerramos el archivo
-
-cerrarArchivo [idarchivo]
-
-
-
-;leemos el archivo
-
-mov eax, 5
-;mov ebx, archivo
-;mov ecx, 0
-;mov edx, 0
-;int 0x80
-cargar 5,archivo
 test eax, eax
 jz salir
 mov dword[idarchivo], eax
-;mov eax, '654'
-;mov [texto],eax
 
-leeTxt [idarchivo], texto, 3
-escribe texto, 3
-
-leeTxt [idarchivo], texto, 3
-escribe texto, 3
+escribe msg2, len2
+mov eax,'pau'
+mov [texto], eax
+escribeTxt [idarchivo],texto,3
+escribe texto, 4
 
 
+
+
+
+;cerramos el archivo
 cerrarArchivo [idarchivo]
+
+
 
 
 salir:
